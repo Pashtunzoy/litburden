a2ed7e05c6e638bbf3229bb39bc324e896eac63f
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-04 01:12:12 +1000

    Removed the heroku db test

--- database/migrations/2017_05_03_103650_create_posts_table.php
+++ database/migrations/2017_05_03_103650_create_posts_table.php
@@ -19,7 +19,6 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('location');
             $table->string('image-url');
-            $table->string('email');
             $table->string('want');
             $table->string('give');
             $table->text('body');
