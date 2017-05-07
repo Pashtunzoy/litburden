4b8bff1e88ab681e467b8cd760026c126350e6e7
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-07 16:35:32 +1000

    Post now will be displayed from desc based on when created

--- app/Post.php
+++ app/Post.php
@@ -1,12 +1,26 @@
 <?php
 
 namespace App;
+
 use App\User;
+use Carbon\Carbon;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     public function user() {
       return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        if ($month = $filters['month']) {
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if ($year = $filters['year']) {
+            $query->whereYear('created_at', $year);
+        }
     }
 }
