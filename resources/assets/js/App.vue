efddc06139471ce731b28494fa96c0463736c1f9
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-08 20:12:48 +1000

    VueJs barebone setup

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="">
+    <h1>Hello from APP.VUE</h1>
+  </div>
+</template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,22 +4,19 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Litburden</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <script>
-            window.Laravel = <?php echo json_encode([
-                'csrfToken' => csrf_token(),
-            ]); ?>
-        </script>
+        <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     </head>
     <body>
         <div id='app'>
             <example></example>
         </div>
 
-        <script src="/js/app.js"></script>
+        <script src="{{mix('/js/app.js')}}"></script>
+        <script>
+            window.Laravel = <?php echo json_encode([
+                'csrfToken' => csrf_token(),
+            ]); ?>
+        </script>
     </body>
 </html>
