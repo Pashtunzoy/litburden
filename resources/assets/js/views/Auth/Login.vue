fedc22327492dcf174d1ff61be6a7514e92fa3b9
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-10 21:57:52 +1000

    Added validation to login form and backend communication

--- resources/assets/js/views/Auth/Login.vue
+++ resources/assets/js/views/Auth/Login.vue
@@ -7,34 +7,33 @@
 
                  <div class="field">
                    <p class="control has-icons-left">
-                     <input class="input" type="email" placeholder="Email">
+                     <input v-bind:class="{'is-danger': error.email, input: true}" type="email" placeholder="Email"
+                     v-model="form.email">
                      <span class="icon is-small is-left">
                        <i class="fa fa-envelope"></i>
                      </span>
                    </p>
+                    <p class="help is-danger" v-if="error.email">
+                        {{error.email[0]}}
+                    </p>
                  </div>
 
                  <div class="field">
                    <p class="control has-icons-left">
-                     <input class="input" type="password" placeholder="Password">
+                     <input v-bind:class="{'is-danger': error.password, input: true}" type="password" placeholder="Password"
+                     v-model="form.password">
                      <span class="icon is-small is-left">
                        <i class="fa fa-lock"></i>
                      </span>
                    </p>
-                 </div>
-
-                 <div class="field">
-                   <p class="control has-icons-left">
-                     <input class="input" type="password" placeholder="Password Confirmation">
-                     <span class="icon is-small is-left">
-                       <i class="fa fa-lock"></i>
-                     </span>
+                   <p class="help is-danger" v-if="error.password">
+                       {{error.password[0]}}
                    </p>
                  </div>
 
                  <div class="field">
                    <p class="control">
-                     <button class="button is-success">
+                     <button v-bind:class="{'is-loading': isFetching, 'button is-success': true}">
                        Login
                      </button>
                    </p>
@@ -46,15 +45,37 @@
   </div>
 </template>
 <script>
+import { post } from '../../helpers/api';
+
 export default {
     data () {
         return {
-                hello: true
+            form: {
+                email: '',
+                password: ''
+            },
+            error: {},
+            isFetching: false
         }
     },
     methods: {
       login () {
-        console.log('you logged in');
+        this.isFetching = true;
+        this.error = {};
+        post(`/api/v1/login`, this.form)
+            .then((res) => {
+                if (res.data.token) {
+                    this.$router.push('/');
+                }
+                this.isFetching = false;
+            })
+            .catch((err) => {
+                if (err) {
+                    this.error = err.response.data;
+                    console.log(err.response.data);
+                }
+                this.isFetching = false;
+            })
       }
     }
 }
