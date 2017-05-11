c979762f97bf9b7f820e3edd3f7ee19a8abdce5f
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-11 21:23:22 +1000

    Saperated the Search bar to it's own component

---
+++ resources/assets/js/components/SearchBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="field has-addons has-addons-centered">
+      <p class="control is-expanded">
+        <input class="input is-large" type="text" placeholder="I'm looking for...">
+      </p>
+      <p class="control">
+        <a class="button is-primary is-large">
+         <i class="fa fa-search" aria-hidden="true"></i>
+        </a>
+      </p>
+    </div>
+</template>
