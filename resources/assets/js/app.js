b76dd3b31ba65d2083a6a9c1bcaf95774bc47776
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-11 21:21:40 +1000

    Registered components with the app

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,13 @@ import Vue from 'vue';
 import router from './router';
 import App from './App.vue';
 import Navbar from './components/Navbar.vue';
+import SearchBar from './components/SearchBar.vue';
+import Footer from './components/Footer.vue';
 
 Vue.component('Navbar', Navbar);
+Vue.component('SearchBar', SearchBar);
+Vue.component('Footer', Footer);
+
 const app = new Vue({
     el: '#app',
     template: `<app></app>`,
