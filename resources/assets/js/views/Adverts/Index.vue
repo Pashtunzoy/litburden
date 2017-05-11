7e48fff1ac8f377fd9aa9610ee05c6c54cbc8d5b
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-11 21:24:18 +1000

    Added the main component to view all the adverts but for now it's empty'

---
+++ resources/assets/js/views/Adverts/Index.vue
@@ -0,0 +1,25 @@
+<template v-if="authentic">
+  <div id="">
+    <h1>From Index</h1>
+  </div>
+</template>
+
+<script>
+    import auth from '../../store/auth';
+
+    export default {
+      data: function data() {
+        return {
+            auth: auth.state
+        }
+      },
+      computed: {
+          authentic() {
+              if (auth.api_token && auth.user_id) {
+                  return true;
+              }
+              return false;
+          }
+      }
+    }
+</script>
