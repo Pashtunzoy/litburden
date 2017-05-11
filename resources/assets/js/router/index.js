9a9a19adeb4e340a0bc5f07c54a96286e1e65213
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-11 21:22:17 +1000

    Added some new routes

--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Main from '../views/Main.vue';
 import Register from '../views/Auth/Register.vue';
 import Login from '../views/Auth/Login.vue';
+import Index from '../views/Adverts/Index.vue';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     routes: [
+        {path: '/', component: Main},
         {path: '/register', component: Register},
-        {path: '/login', component: Login}
+        {path: '/login', component: Login},
+        {path: '/ads', component: Index}
     ]
 });
 
