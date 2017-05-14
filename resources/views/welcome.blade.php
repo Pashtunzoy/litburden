5c673f68b2fefbcaebf0b343dafc7d4d6d845cfc
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-14 18:10:14 +1000

    Removed bulma.io for Bootstrap as it wasn't a good fit for me atm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,11 @@
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <title>Litburden</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     </head>
     <body>
-        <div id='app'></div>
+        <section id='app'></section>
 
         <script src="{{mix('/js/app.js')}}"></script>
         <script>
