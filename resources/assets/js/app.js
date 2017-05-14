68d7e227bc52fab84a1b430837d0a25c45c71f21
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-14 18:12:42 +1000

    Added a new component for the adverts show all on home page

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,10 +3,12 @@ import router from './router';
 import App from './App.vue';
 import Navbar from './components/Navbar.vue';
 import SearchBar from './components/SearchBar.vue';
+import MainContent from './components/MainContent.vue';
 import Footer from './components/Footer.vue';
 
 Vue.component('Navbar', Navbar);
 Vue.component('SearchBar', SearchBar);
+Vue.component('MainContent', MainContent);
 Vue.component('Footer', Footer);
 
 const app = new Vue({
---
+++ resources/assets/js/components/MainContent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="card">
+            <img
+                class="card-img-top"
+                style="height: 280px;width: 100%;display: block;" :src="advert['image-url']"
+                alt="advert image">
+            <div class="card-block">
+                <p class="card-text">
+                    {{advert.title}}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['advert']
+}
+</script>
