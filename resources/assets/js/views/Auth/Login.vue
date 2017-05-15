8c062c3ad1eef8c16ef66a397b72bf5898dacb27
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-15 15:36:37 +1000

    Fixed the login and registered form pages and added custome css for the design of form

--- resources/assets/js/views/Auth/Login.vue
+++ resources/assets/js/views/Auth/Login.vue
@@ -1,47 +1,32 @@
 <template>
   <div class="container">
-      <div class="column is-half is-offset-one-quarter">
-          <div class="box">
-              <h3 class="title is-3">Login</h3>
-             <form @submit.prevent="login">
+      <form @submit.prevent="login" class="form-auth">
+          <h2 class="form-signin-heading">Please Login</h2>
+           <label for="inputEmail" class="sr-only">Email address</label>
+           <input
+               type="email"
+               v-bind:class="{'form-control-danger': error.email, 'form-control': true}"
+               id="inputEmail"
+               placeholder="Emai"
+               v-model="form.email"
+               required
+               autofocus
+           >
 
-                 <div class="field">
-                   <p class="control has-icons-left">
-                     <input v-bind:class="{'is-danger': error.email, input: true}" type="email" placeholder="Email"
-                     v-model="form.email">
-                     <span class="icon is-small is-left">
-                       <i class="fa fa-envelope"></i>
-                     </span>
-                   </p>
-                    <p class="help is-danger" v-if="error.email">
-                        {{error.email[0]}}
-                    </p>
-                 </div>
+           <label for="inputPassword" class="sr-only">Password</label>
+           <input
+               type="password"
+               v-bind:class="{'form-control-danger': error.password, 'form-control': true}"
+               id="inputPassword"
+               placeholder="Password"
+               v-model="form.password"
+               required
+               autofocus
+           >
 
-                 <div class="field">
-                   <p class="control has-icons-left">
-                     <input v-bind:class="{'is-danger': error.password, input: true}" type="password" placeholder="Password"
-                     v-model="form.password">
-                     <span class="icon is-small is-left">
-                       <i class="fa fa-lock"></i>
-                     </span>
-                   </p>
-                   <p class="help is-danger" v-if="error.password">
-                       {{error.password[0]}}
-                   </p>
-                 </div>
+           <button :class="{'loading': isFetching, 'btn btn-lg btn-primary btn-block': true}" type="submit">Log in</button>
 
-                 <div class="field">
-                   <p class="control">
-                     <button v-bind:class="{'is-loading': isFetching, 'button is-success': true}">
-                       Login
-                     </button>
-                   </p>
-                 </div>
-
-             </form>
-          </div>
-      </div>
+      </form>
   </div>
 </template>
 <script>
@@ -70,13 +55,17 @@ export default {
                 if (res.data.token) {
                     auth.set(res.data.token, res.data.user_id);
                     flashMessage.setSuccess('You have successfuly logged In!');
+                    flashMessage.removeError();
                     this.$router.push('/');
                 }
                 this.isFetching = false;
             })
             .catch((err) => {
                 if (err) {
-                    flashMessage.setError('Sorry, Something went wrong while loggin In!');
+                    const msg = err.response.data.msg;
+                    msg ?
+                        flashMessage.setError(msg): flashMessage.setError('Sorry, Something went wrong while loggin In!');
+                    flashMessage.removeSuccess();
                     this.error = err.response.data;
                 }
                 this.isFetching = false;
--- resources/assets/js/views/Auth/Register.vue
+++ resources/assets/js/views/Auth/Register.vue
@@ -1,71 +1,54 @@
 <template>
-    <div class="column is-half is-offset-one-quarter">
-      <div class="box">
-          <h3 class="title is-3">Create An Account</h3>
-          <form @submit.prevent="register">
+    <div class="container">
+        <form @submit.prevent="register" class="form-auth">
+            <h2 class="form-signin-heading">Please Register</h2>
+            <label for="inputName" class="sr-only">Full Name</label>
+            <input
+                type="text"
+                v-bind:class="{'form-control-danger': error.name, 'form-control': true}"
+                id="inputName"
+                placeholder="Full Name"
+                v-model="form.name"
+                required
+                autofocus
+            >
 
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-bind:class="{'is-danger': error.name, input: true}" type="text" placeholder="Full Name"
-                  v-model="form.name">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-id-card" aria-hidden="true"></i>
-                  </span>
-                </p>
-                <p class="help is-danger" v-if="error.name">
-                    {{error.name[0]}}
-                </p>
-              </div>
+             <label for="inputEmail" class="sr-only">Email address</label>
+             <input
+                 type="email"
+                 v-bind:class="{'form-control-danger': error.email, 'form-control': true}"
+                 id="inputEmail"
+                 placeholder="Emai"
+                 v-model="form.email"
+                 required
+                 autofocus
+             >
 
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-bind:class="{'is-danger': error.email, input: true}" type="email" placeholder="Email"
-                  v-model="form.email">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
-                  </span>
-                </p>
-                <p class="help is-danger" v-if="error.email">
-                    {{error.email[0]}}
-                </p>
-              </div>
+             <label for="inputPassword" class="sr-only">Password</label>
+             <input
+                 type="password"
+                 v-bind:class="{'form-control-danger': error.password, 'form-control': true}"
+                 id="inputPassword"
+                 placeholder="Password"
+                 v-model="form.password"
+                 required
+                 autofocus
+             >
 
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-bind:class="{'is-danger': error.password, input: true}" type="password" placeholder="Password"
-                  v-model="form.password">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
-                  </span>
-                </p>
-                <p class="help is-danger" v-if="error.password">
-                    {{error.password[0]}}
-                </p>
-              </div>
+             <label for="inputPassword_confrimation" class="sr-only">Password</label>
+             <input
+                 type="password"
+                 v-bind:class="{'form-control-danger': error.password_confirmation, 'form-control': true}"
+                 id="inputPassword_confrimation"
+                 placeholder="Password"
+                 v-model="form.password_confirmation"
+                 required
+                 autofocus
+             >
 
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-bind:class="{'is-danger': error.password_confirmation, input: true}" type="password" placeholder="Password Confirmation"
-                  v-model="form.password_confirmation">
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
-                  </span>
-                </p>
-                <p class="help is-danger" v-if="error.password_confirmation">
-                    {{error.password_confirmation[0]}}
-                </p>
-              </div>
+             <button :class="{'loading': isFetching, 'btn btn-lg btn-primary btn-block': true}" type="submit">Register</button>
 
-              <div class="field">
-                <p class="control">
-                  <button v-bind:class="{'is-loading': isFetching, 'button is-success': true}">
-                    Register
-                  </button>
-                </p>
-              </div>
-
-          </form>
-      </div>
+        </form>
     </div>
 </template>
 
