ec259decfa07a5a469924b2f4010cd4992434120
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-15 15:32:29 +1000

    Changed the component name from Main to Home for readiblity

---
+++ resources/assets/js/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <section class="jumbotron jumbotron-fluid">
+            <!-- Hero content: will be in the middle -->
+              <div class="container">
+                <h1 class="display-3 text-center">Share. Give. Win.</h1>
+                <p class="lead text-center">Australia's vibrant community one stop</p>
+              </div>
+
+            <!-- Hero footer: will stick at the bottom -->
+            <SearchBar></SearchBar>
+        </section>
+        <div class="album text-muted">
+            <div class="container">
+                <div class="row">
+                    <MainContent v-for="advert in this.adverts" :advert="advert" :key="advert.id"></MainContent>
+                </div>
+            </div>
+        </div>
+        <section class="container">
+            <div class="columns">
+                <div class="column is-half is-offset-one-quarter">
+                    <router-link to="/ads" class="btn btn-lg">See all</router-link>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import { get } from '../helpers/api';
+import flashMessage from '../helpers/flashMessage';
+
+export default {
+    created() {
+        get('/api/v1/ads')
+            .then((res) => {
+                if (res.data && res.status === 200) {
+                    this.adverts = res.data;
+                }
+            })
+            .catch((err) => {
+                flashMessage.error('Something went wrong!')
+            });
+    },
+    data() {
+        return {
+            adverts: []
+        }
+    }
+}
+</script>
+<style>
+</style>
