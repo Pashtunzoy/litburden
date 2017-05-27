5d023e29b1d07bf783447cef570e92156c127faf
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-05-27 18:17:14 +1000

    Added the neccassary classes and tags for the show page still WIP

--- resources/assets/js/views/Adverts/Show.vue
+++ resources/assets/js/views/Adverts/Show.vue
@@ -1,7 +1,38 @@
 <template>
-    <div>
-        {{ adverts.title }}
-        <img :src="adverts['image-url']" alt="">
+    <div class="container">
+        <SearchBar></SearchBar>
+        <div class="row">
+          <div class="col-md-8">
+            <div class="card">
+                <div class="card-block">
+                    <h4 class="card-title">{{ advert.title }}</h4>
+                    <div class="row">
+                        <p class="col-md-8"><i class="show--card__map-icon fa fa-map-marker" aria-hidden="true"></i> {{ advert.location }}</p>
+                        <p class="col-md-4 card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <img class="img-fluid show--img mx-auto d-block" :src="advert['image-url']" alt="Advertisment Image">
+                <div class="card-block show--card__advertiser-description">
+                    <h4 class="card-title">Advertiser's description</h4>
+                    <div class="row">
+                        <p class="col card-text">Want: <p>{{ advert.want }}</p></p>
+                        <p class="col card-text">Give: <p>{{ advert.give }}</p></p>
+                    </div>
+                    <p class="card-text">Description: </p>
+                    <p>{{ advert.body}}</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+          </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-block">
+                  <h4 class="card-title">Advertiser's Info</h4>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.1
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
 </template>
 
@@ -12,8 +43,8 @@ export default {
     created() {
         get(`api/v1/ads/${this.$route.params.id}`)
             .then((res) => {
-                this.adverts = res.data;
-                console.log(this.adverts);
+                this.advert = res.data;
+                console.log(this.advert);
             })
             .catch((err) => {
                 console.log(err);
@@ -21,7 +52,7 @@ export default {
     },
     data() {
         return {
-            adverts: {}
+            advert: {}
         }
     }
 }
