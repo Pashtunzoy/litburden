99d69c2b79f6738eca9aa3efb95e4f21cd31c616
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-06-11 17:43:42 +1000

    Removed algolia search functionality with Elastsearch

--- resources/assets/js/views/Adverts/Index.vue
+++ resources/assets/js/views/Adverts/Index.vue
@@ -6,18 +6,16 @@
 
 <script>
 import MainContent from '../../components/MainContent.vue';
-
-const client = algoliasearch('44UZY1A1LY', '0d519a5c02efb12fa9918fc56454e360');
-const index = client.initIndex('ads');
+import { get } from '../../helpers/api';
 
 export default {
     mounted() {
         const searchText = this.$route.query.search;
         if (searchText) {
-            index.search(searchText)
-                .then((content) => {
-                    if (content) {
-                        return this.adverts = content.hits;
+            get(`/api/v1/search?q=${searchText}`)
+                .then((res) => {
+                    if (res) {
+                        return this.adverts = res.data;
                     }
                 })
                 .catch((err) => {
