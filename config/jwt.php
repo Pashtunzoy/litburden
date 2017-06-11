8a5982274701e1fd24172b05f12ee093af17e9a3
Pashtunzoy <asifkhannoorzai@gmail.com>
2017-06-11 17:41:05 +1000

    Added secret env variable

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'xGem9ygsJ0tRKtukuZgEHq34ZirDOYrp'),
+    'secret' => env('JWT_SECRET', 'litburden'),
 
     /*
     |--------------------------------------------------------------------------
